1178375982b55b0a36ce7281fe15242c31ff0474
Yalerooo <Yaleros2@gmail.com>
2024-03-12 17:43:28 +0100

    Igris updated

--- src/main/java/net/porcosland/porcosmod/entity/IgrisEntity.java
+++ src/main/java/net/porcosland/porcosmod/entity/IgrisEntity.java
@@ -1,12 +1,16 @@
 
 package net.porcosland.porcosmod.entity;
 
+import net.porcosland.porcosmod.procedures.IgrisOnInitialEntitySpawnProcedure;
+import net.porcosland.porcosmod.procedures.IgrisItIsStruckByLightningProcedure;
+import net.porcosland.porcosmod.procedures.IgrisEntityDiesProcedure;
 import net.porcosland.porcosmod.init.PorcosmodModEntities;
 
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.network.PlayMessages;
 import net.minecraftforge.network.NetworkHooks;
 
+import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
@@ -16,18 +20,26 @@ import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.MobType;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.LightningBolt;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerBossEvent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.chat.Component;
+import net.minecraft.nbt.CompoundTag;
+
+import javax.annotation.Nullable;
 
 public class IgrisEntity extends Monster {
 	private final ServerBossEvent bossInfo = new ServerBossEvent(this.getDisplayName(), ServerBossEvent.BossBarColor.RED, ServerBossEvent.BossBarOverlay.PROGRESS);
@@ -78,6 +90,25 @@ public class IgrisEntity extends Monster {
 	@Override
 	public SoundEvent getDeathSound() {
 		return ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.generic.death"));
+	}
+
+	@Override
+	public void thunderHit(ServerLevel serverWorld, LightningBolt lightningBolt) {
+		super.thunderHit(serverWorld, lightningBolt);
+		IgrisItIsStruckByLightningProcedure.execute(this);
+	}
+
+	@Override
+	public void die(DamageSource source) {
+		super.die(source);
+		IgrisEntityDiesProcedure.execute(this.level());
+	}
+
+	@Override
+	public SpawnGroupData finalizeSpawn(ServerLevelAccessor world, DifficultyInstance difficulty, MobSpawnType reason, @Nullable SpawnGroupData livingdata, @Nullable CompoundTag tag) {
+		SpawnGroupData retval = super.finalizeSpawn(world, difficulty, reason, livingdata, tag);
+		IgrisOnInitialEntitySpawnProcedure.execute(world, this);
+		return retval;
 	}
 
 	@Override
