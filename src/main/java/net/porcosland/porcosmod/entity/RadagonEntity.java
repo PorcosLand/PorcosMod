e8be0f5a7febe61c958413d6141cdad7a3a527fa
firestarfox <inigosanchezarriaga@gmail.com>
2024-03-12 23:59:56 +0100

    mejorar radagon

--- src/main/java/net/porcosland/porcosmod/entity/RadagonEntity.java
+++ src/main/java/net/porcosland/porcosmod/entity/RadagonEntity.java
@@ -158,9 +158,9 @@ public class RadagonEntity extends Monster {
 	public static AttributeSupplier.Builder createAttributes() {
 		AttributeSupplier.Builder builder = Mob.createMobAttributes();
 		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.3);
-		builder = builder.add(Attributes.MAX_HEALTH, 10);
-		builder = builder.add(Attributes.ARMOR, 0);
-		builder = builder.add(Attributes.ATTACK_DAMAGE, 3);
+		builder = builder.add(Attributes.MAX_HEALTH, 500);
+		builder = builder.add(Attributes.ARMOR, 3.5);
+		builder = builder.add(Attributes.ATTACK_DAMAGE, 10);
 		builder = builder.add(Attributes.FOLLOW_RANGE, 16);
 		return builder;
 	}
